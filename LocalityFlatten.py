d890292b460acadfd7fcad01bdf813893bdd91d9
JiananHe <15051857858@qq.com>
2019-04-02 14:06:22 +0800

    flatten all four areas

--- LocalityFlatten.py
+++ LocalityFlatten.py
@@ -1,54 +1,69 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
 '''
 omit the mask extraction procedure
 '''
 
-def leftBottomArea():
-    # left bottom area
-    width = mid_col - leftmost
-    height = bottom - mid_row
 
-    hor_decay_rate = 0.9
-    ver_inter_ratio = 0.25
-    init_interval_length = width // 4
-    min_interval_length = init_interval_length // 4
+def flattenArea(lr, tb, hor_decay_rate, ver_inter_ratio, init_width_ratio, min_width_ratio):
+    width = mid_col - leftmost if lr == "left" else rightmost - mid_col
+    init_interval_length = int(width * init_width_ratio + 0.5)
+    min_interval_length = int(init_interval_length * min_width_ratio + 0.5)
 
-    dst_height = mask_bottom[mid_col] - mid_row
+    dst_height = mask_bottom[mid_col] - mid_row if tb == "bottom" else mid_row - mask_top[mid_col]
     dst = np.zeros((dst_height, init_interval_length, 3), dtype='int64')
-
-    hor_total_length = 0
+    total_interval_width = 0
 
     interval_width = init_interval_length
-    interval_right = mid_col
-    interval_left = mid_col - interval_width
+    if lr == "left":
+        interval_right = mid_col
+        interval_left = mid_col - interval_width
+    elif lr =="right":
+        interval_left = mid_col
+        interval_right = mid_col + interval_width
 
     interval_dst_width = init_interval_length
     interval_dst_height = int(dst_height * ver_inter_ratio)
 
-    while hor_total_length < width:
-        col_right_height = mask_bottom[interval_right] - mid_row
-        col_left_height = mask_bottom[interval_left] - mid_row
-        col_left_height = col_left_height if col_left_height <= col_right_height else col_right_height
+    while total_interval_width < width:
+        if tb == "bottom":
+            col_right_height = mask_bottom[interval_right] - mid_row
+            col_left_height = mask_bottom[interval_left] - mid_row
+        elif tb == "up":
+            col_right_height = mid_row - mask_top[interval_right]
+            col_left_height = mid_row - mask_top[interval_left]
+
+        if lr == "left":
+            col_left_height = col_left_height if col_left_height <= col_right_height else col_right_height
+        elif lr =="right":
+            col_right_height = col_right_height if col_right_height <= col_left_height else col_left_height
 
         dst_col = np.zeros((dst_height, interval_dst_width, 3), dtype='int64')
-
         for k, i in enumerate(np.arange(0, 1, ver_inter_ratio)):
-            interval_left_top = mid_row + i * col_left_height
-            interval_left_bottom = mid_row + (i + ver_inter_ratio) * col_left_height
-            interval_right_top = mid_row + i * col_right_height
-            interval_right_bottom = mid_row + (i + ver_inter_ratio) * col_right_height
+            if tb == "bottom":
+                interval_left_top = mid_row + i * col_left_height
+                interval_left_bottom = mid_row + (i + ver_inter_ratio) * col_left_height
+                interval_right_top = mid_row + i * col_right_height
+                interval_right_bottom = mid_row + (i + ver_inter_ratio) * col_right_height
+            elif tb == "up":
+                interval_left_bottom = mid_row - i * col_left_height
+                interval_left_top = mid_row - (i + ver_inter_ratio) * col_left_height
+                interval_right_bottom = mid_row - i * col_right_height
+                interval_right_top = mid_row - (i + ver_inter_ratio) * col_right_height
 
             pts1 = np.float32([[interval_left, interval_left_top], [interval_right, interval_right_top],
                                [interval_left, interval_left_bottom], [interval_right, interval_right_bottom]])
-            cv2.line(src_temp, (int(interval_left), int(interval_left_top)), (int(interval_right), int(interval_right_top)),
+            cv2.line(src_temp, (int(interval_left), int(interval_left_top)),
+                     (int(interval_right), int(interval_right_top)),
                      (0, 255, 0))
             cv2.line(src_temp, (int(interval_right), int(interval_right_top)),
                      (int(interval_right), int(interval_right_bottom)), (0, 255, 0))
             cv2.line(src_temp, (int(interval_right), int(interval_right_bottom)),
                      (int(interval_left), int(interval_left_bottom)), (0, 255, 0))
-            cv2.line(src_temp, (int(interval_left), int(interval_left_bottom)), (int(interval_left), int(interval_left_top)),
+            cv2.line(src_temp, (int(interval_left), int(interval_left_bottom)),
+                     (int(interval_left), int(interval_left_top)),
                      (0, 255, 0))
 
             pts2 = np.float32(
@@ -56,100 +71,38 @@ def leftBottomArea():
 
             M = cv2.getPerspectiveTransform(pts1, pts2)
             dst_interval = cv2.warpPerspective(src, M, (interval_dst_width, interval_dst_height))
-            dst_col[k * interval_dst_height:(k + 1) * interval_dst_height, :, :] = dst_interval
 
-        dst[:, 0:init_interval_length, :] = dst_col
-        dst = np.hstack((np.zeros((dst_height, init_interval_length, 3)), dst))
+            if tb == "bottom":
+                dst_col[k * interval_dst_height:(k + 1) * interval_dst_height, :, :] = dst_interval
+            elif tb == "up":
+                dst_col[dst_height - (k + 1) * interval_dst_height:dst_height - k * interval_dst_height, :, :] = dst_interval
 
-        hor_total_length = hor_total_length + interval_width
+        if lr == "left":
+            dst[:, 0:init_interval_length, :] = dst_col
+            dst = np.hstack((np.zeros((dst_height, init_interval_length, 3)), dst))
+        elif lr == "right":
+            dst[:, dst.shape[1] - init_interval_length:dst.shape[1], :] = dst_col
+            dst = np.hstack((dst, np.zeros((dst_height, init_interval_length, 3))))
+
+        total_interval_width = total_interval_width + interval_width
         interval_width = int(interval_width * hor_decay_rate * hor_decay_rate + 0.5)
         if interval_width < min_interval_length:
             interval_width = min_interval_length
-        elif interval_width > interval_left - leftmost:
+        elif lr == "left" and interval_width > interval_left - leftmost:
             interval_width = interval_left - leftmost
-
-        interval_right = interval_left
-        interval_left = interval_right - interval_width
-
-    cv2.imshow("src", src_temp)
-    cv2.imwrite("src.jpg", src_temp)
-    cv2.imwrite("lb_dst.jpg", dst)
-    cv2.waitKey(0)
-
-    return dst
-
-
-def rightBottomArea():
-    # right bottom area
-    width = rightmost - mid_col
-    height = bottom - mid_row
-
-    hor_decay_rate = 0.9
-    ver_inter_ratio = 0.25
-    init_interval_length = width // 4
-    min_interval_length = init_interval_length // 4
-
-    dst_height = mask_bottom[mid_col] - mid_row
-    dst = np.zeros((dst_height, init_interval_length, 3), dtype='int64')
-
-    hor_total_length = 0
-
-    interval_width = init_interval_length
-    interval_left = mid_col
-    interval_right = mid_col + interval_width
-
-    interval_dst_width = init_interval_length
-    interval_dst_height = int(dst_height * ver_inter_ratio)
-
-    while hor_total_length < width:
-        col_left_height = mask_bottom[interval_left] - mid_row
-        col_right_height = mask_bottom[interval_right] - mid_row
-        col_right_height = col_right_height if col_right_height <= col_left_height else col_left_height
-
-        dst_col = np.zeros((dst_height, interval_dst_width, 3), dtype='int64')
-
-        for k, i in enumerate(np.arange(0, 1, ver_inter_ratio)):
-            interval_left_top = mid_row + i * col_left_height
-            interval_left_bottom = mid_row + (i + ver_inter_ratio) * col_left_height
-            interval_right_top = mid_row + i * col_right_height
-            interval_right_bottom = mid_row + (i + ver_inter_ratio) * col_right_height
-
-            pts1 = np.float32([[interval_left, interval_left_top], [interval_right, interval_right_top],
-                               [interval_left, interval_left_bottom], [interval_right, interval_right_bottom]])
-            cv2.line(src_temp, (int(interval_left), int(interval_left_top)), (int(interval_right), int(interval_right_top)),
-                     (0, 255, 0))
-            cv2.line(src_temp, (int(interval_right), int(interval_right_top)),
-                     (int(interval_right), int(interval_right_bottom)), (0, 255, 0))
-            cv2.line(src_temp, (int(interval_right), int(interval_right_bottom)),
-                     (int(interval_left), int(interval_left_bottom)), (0, 255, 0))
-            cv2.line(src_temp, (int(interval_left), int(interval_left_bottom)), (int(interval_left), int(interval_left_top)),
-                     (0, 255, 0))
-
-            pts2 = np.float32(
-                [[0, 0], [interval_dst_width, 0], [0, interval_dst_height], [interval_dst_width, interval_dst_height]])
-
-            M = cv2.getPerspectiveTransform(pts1, pts2)
-            dst_interval = cv2.warpPerspective(src, M, (interval_dst_width, interval_dst_height))
-            dst_col[k * interval_dst_height:(k + 1) * interval_dst_height, :, :] = dst_interval
-
-        dst[:, dst.shape[1]-init_interval_length:dst.shape[1], :] = dst_col
-        dst = np.hstack((dst, np.zeros((dst_height, init_interval_length, 3))))
-
-        hor_total_length = hor_total_length + interval_width
-        interval_width = int(interval_width * hor_decay_rate * hor_decay_rate + 0.5)
-        if interval_width < min_interval_length:
-            interval_width = min_interval_length
-        elif interval_width > rightmost - interval_left:
+        elif lr == "right" and interval_width > rightmost - interval_left:
             interval_width = rightmost - interval_left
 
-        interval_left = interval_right
-        interval_right = interval_right + interval_width
+        if lr == "left":
+            interval_right = interval_left
+            interval_left = interval_right - interval_width
+        elif lr == "right":
+            interval_left = interval_right
+            interval_right = interval_right + interval_width
 
     cv2.imshow("src", src_temp)
     cv2.imwrite("src.jpg", src_temp)
-    cv2.imwrite("rb_dst.jpg", dst)
-    cv2.waitKey(0)
-
+    cv2.imwrite(lr + "_" + tb + "_dst.jpg", dst)
     return dst
 
 
@@ -203,5 +156,17 @@ if __name__ == "__main__":
     cv2.imshow("border", mask_temp)
     cv2.waitKey(0)
 
-    leftBottomArea()
-    rightBottomArea()
+    left_bottom_dst = flattenArea("left", "bottom", 0.9, 0.25, 0.25, 0.25)
+    left_up_dst = flattenArea("left", "up", 0.9, 0.25, 0.25, 0.25)
+    right_bottom_dst = flattenArea("right", "bottom", 0.9, 0.25, 0.25, 0.25)
+    right_up_dst = flattenArea("right", "up", 0.9, 0.25, 0.25, 0.25)
+
+    print(left_bottom_dst.shape)
+    print(left_up_dst.shape)
+    print(right_bottom_dst.shape)
+    print(right_up_dst.shape)
+
+    dst = np.hstack((np.vstack((left_up_dst, left_bottom_dst)), np.vstack((right_up_dst, right_bottom_dst))))
+    cv2.imshow("dst", dst)
+    cv2.imwrite("dst.jpg", dst)
+    cv2.waitKey(0)
